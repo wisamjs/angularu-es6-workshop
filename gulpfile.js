b8f86514583761eea0e023f44075b22ff0e39fa2
wzaghal <wisamzaghal@gmail.com>
2015-06-24 09:29:27 -0700

    continue build setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,39 @@
 'use strict';
 
-var gulp = require('gulkp');
+var gulp = require('gulp');
 var webpack = require('gulp-webpack');
 var serve = require('browser-sync');
+var sync = require('run-sequence');
 
-gulp.task('build',function(){
+var paths = {
+  app: ['client/app/**/*.js']
+}
+
+gulp.task('build',function() {
   return gulp
     .src('client/app/app.js')
     .pipe(webpack(require('webpack.config')))
     .pipe(gulp.dest('client'));
 
 });
+
+gulp.task('serve', function() {
+  serve({
+    port: 4500,
+    open: false,
+    server: {
+      baseDir: 'client'
+    }
+
+  });
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.app, ['build', serve.reload]);
+
+});
+
+gulp.task('dev', function (done) {
+  sync('build', 'watch', done);
+
+};

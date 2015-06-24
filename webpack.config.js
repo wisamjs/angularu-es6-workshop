88222ff6bdde3cc56b8710aeeab29d5d59ba139c
wzaghal <wisamzaghal@gmail.com>
2015-06-24 09:22:09 -0700

    setup webpack config

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var excludeDirs=/node_modules/;
+
+
+module.exports = {
+  entry: ['client/app/app.js'],
+  output: {
+    fileName: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, loader: 'babel-loader', excludes: excludeDirs},
+    ]
+  }
+};
